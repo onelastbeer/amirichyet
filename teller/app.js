630821948e848dd15af7fd2c035a152998389d7c
Louis Merlin <louis.merlin@epfl.ch>
2017-06-06 18:30:07 +0200

    Change up stuff, api call

--- teller/app.js
+++ teller/app.js
@@ -2,15 +2,23 @@ new Vue({
   el: '#app',
   data: {
     greeting: 'Welcome to your Vue.js app!',
+    message: 'Waiting for transmission',
     docsURL: 'http://vuejs.org/guide/',
     gitterURL: 'https://gitter.im/vuejs/vue',
     forumURL: 'http://forum.vuejs.org/'
+  },
+  created: function() {
+    this.fetchMessage()
   },
   methods: {
     humanizeURL: function(url) {
       return url
         .replace(/^https?:\/\//, '')
         .replace(/\/$/, '')
+    },
+    fetchMessage: function() {
+      var v = this;
+      get('/api/', function(data) { v.message = data })
     }
   }
 })
