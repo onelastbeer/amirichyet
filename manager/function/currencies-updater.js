112adb61364473002d78da48786213e58c39a4f5
Louis Merlin <louis.merlin@epfl.ch>
2017-06-28 19:55:23 +0200

    Fix currency updater

--- manager/function/currencies-updater.js
+++ manager/function/currencies-updater.js
@@ -1,26 +1,27 @@
 const mongoose = require('mongoose');
 const https = require('https');
-const Currency = require('../schema/currency.js');
+//const Currency = require('../schema/currency.js');
+const Currency = mongoose.model('Currency');
 
-module.exports = function() {
+module.exports = () => {
   console.log('API call:');
   try {
-    https.get('https://api.coinmarketcap.com/v1/ticker/', function(res) {
-      res.on('data', function (data) {
-        /*for(var i=0; i < data.length; i++) {
-          new Currency({
-            name: data[i].name,
-            symbol: data[i].symbol
-          }).save();
-          console.log(data[i].name + " updated");
-        }*/
-        console.log(data);
-      });
-
-    }).on('error', function(error) {
-        console.log(error.message);
+    https.get('https://api.coinmarketcap.com/v1/ticker/', (res) => {
+      var currs = '';
+      res.on('data', (d) => {
+        currs += d;
+      })
+      res.on('end', () => {
+        var parsed = JSON.parse(currs);
+        for (var i = 0; i < parsed.length; i++) {
+          new Currency({name: parsed[i].name, symbol: parsed[i].symbol}).save();
+          console.log(parsed[i].name + " updated (" + parsed[i].rank + ")");
+        }
+      })
+    }).on('error', (error) => {
+      console.log(error.message);
     });
-  } catch(e) {
-      console.log(e);
+  } catch (e) {
+    console.log(e);
   }
 }
