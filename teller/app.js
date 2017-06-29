63b769f1d05e18e396593ce5088650364549570a
Louis Merlin <louis.merlin@epfl.ch>
2017-06-29 11:30:50 +0200

    Add frontend router

--- teller/app.js
+++ teller/app.js
@@ -1,4 +1,22 @@
+Vue.use(VueRouter)
+
+// 2. Define route components
+const Home = { template: '<div>home</div>' }
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+// 3. Create the router
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: Home },
+    { path: '/foo', component: Foo },
+    { path: '/bar', component: Bar }
+  ]
+})
+/*
 new Vue({
+  router,
   el: '#app',
   data: {
     message: 'Waiting for transmission',
@@ -21,4 +39,24 @@ new Vue({
       get('/api/secure/cur', function(data) { v.secureMessage = data });
     }
   }
-})
+})*/
+// 4. Create and mount root instance.
+// Make sure to inject the router.
+// Route components will be rendered inside <router-view>.
+new Vue({
+  router,
+  template: `
+    <div id="app">
+      <h1>Basic</h1>
+      <ul>
+        <li><router-link to="/">/</router-link></li>
+        <li><router-link to="/foo">/foo</router-link></li>
+        <li><router-link to="/bar">/bar</router-link></li>
+        <router-link tag="li" to="/bar" :event="['mousedown', 'touchstart']">
+          <a>/bar</a>
+        </router-link>
+      </ul>
+      <router-view class="view"></router-view>
+    </div>
+  `
+}).$mount('#app')
