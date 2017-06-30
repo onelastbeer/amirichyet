c915c2caf006c24eeadd2d2ec94e6049db0e8868
cyclohexanol <ambroise.mean@gmail.com>
2017-06-30 12:47:55 +0200

    signup hooked

--- teller/src/api/manager.js
+++ teller/src/api/manager.js
@@ -1,10 +1,19 @@
 export default {
   login (username, password, cb, errorCb) {
     this.$http.get('./api/public/user/authenticate').then(response => {
-      this.someData = response.body;
+      this.loginResponse = response.body;
       cb();
     }, response => {
-      errorCb;
+      errorCb();
+    });
+  }
+
+  signup (user, cb, errorCb) {
+    this.$http.get('./api/public/user/new', {user: user}).then(response => {
+      this.signupResponse = response.body;
+      cb();
+    }, response => {
+      errorCb();
     });
   }
 }
