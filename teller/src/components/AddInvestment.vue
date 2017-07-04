4ebb4b3a0feae0d6cfc0bf01303ae316546ed5b9
cyclohexanol <ambroise.mean@gmail.com>
2017-07-04 17:01:56 +0200

    better transaction form rendering

--- teller/src/components/AddInvestment.vue
+++ teller/src/components/AddInvestment.vue
@@ -12,39 +12,39 @@
             <h3 class="color-1">Amounts</h3>
           </div>
           <div class="column">
-            <button class="button button-1" type="button" name="autofill">Auto Fill Amounts</button>
+            <button class="button button-1" type="button" name="autofill">Auto Fill Amounts *</button>
           </div>
         </div>
         <div class="row">
-          <div class="column">
-            <input placeholder="USD Rate" v-model="investment.amounts.usd" type="number">
-          </div>
           <div class="column">
             USD
           </div>
+          <div class="column">
+            <input placeholder="USD Rate" v-model="investment.amounts.usd" type="number">
+          </div>
         </div>
         <div class="row">
-          <div class="column">
-            <input placeholder="EUR Rate" v-model="investment.amounts.eur" type="number">
-          </div>
           <div class="column">
             EUR
           </div>
+          <div class="column">
+            <input placeholder="EUR Rate" v-model="investment.amounts.eur" type="number">
+          </div>
         </div>
         <div class="row">
-          <div class="column">
-            <input placeholder="ETH Rate" v-model="investment.amounts.eth" type="number">
-          </div>
           <div class="column">
             ETH
           </div>
+          <div class="column">
+            <input placeholder="ETH Rate" v-model="investment.amounts.eth" type="number">
+          </div>
         </div>
         <div class="row">
           <div class="column">
-            <input placeholder="BTC Rate" v-model="investment.amounts.btc" type="number">
+            BTC
           </div>
           <div class="column">
-            BTC
+            <input placeholder="BTC Rate" v-model="investment.amounts.btc" type="number">
           </div>
         </div>
         <div class="row">
--- teller/src/components/AddTransaction.vue
+++ teller/src/components/AddTransaction.vue
@@ -20,39 +20,39 @@
             <h3 class="color-1">Rates</h3>
           </div>
           <div class="column">
-            <button class="button button-1" type="button" name="autofill">Auto Fill Rates</button>
+            <button class="button button-1" type="button" name="autofill">Auto Fill Rates *</button>
           </div>
         </div>
         <div class="row">
-          <div class="column">
-            <input placeholder="USD Rate" v-model="transaction.rates.usd" type="number">
-          </div>
           <div class="column">
             USD
           </div>
+          <div class="column">
+            <input placeholder="USD Rate" v-model="transaction.rates.usd" type="number">
+          </div>
         </div>
         <div class="row">
-          <div class="column">
-            <input placeholder="EUR Rate" v-model="transaction.rates.eur" type="number">
-          </div>
           <div class="column">
             EUR
           </div>
+          <div class="column">
+            <input v-model="transaction.rates.eur" type="number">
+          </div>
         </div>
         <div class="row">
-          <div class="column">
-            <input placeholder="ETH Rate" v-model="transaction.rates.eth" type="number">
-          </div>
           <div class="column">
             ETH
           </div>
+          <div class="column">
+            <input placeholder="ETH Rate" v-model="transaction.rates.eth" type="number">
+          </div>
         </div>
         <div class="row">
           <div class="column">
-            <input placeholder="BTC Rate" v-model="transaction.rates.btc" type="number">
+            BTC
           </div>
           <div class="column">
-            BTC
+            <input placeholder="BTC Rate" v-model="transaction.rates.btc" type="number">
           </div>
         </div>
         <div class="row">
